60945e1d09a9d0c302269cd1ce6507728d840c44
jamcarvalho <jamcarvalho@gmail.com>
2023-09-11 16:03:49 +0100

    components dynamics properties - two pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":"aaa",
+    "photoCover":"https://th.bing.com/th/id/OIP.CbdzSV3wjBjHBk13IXcECAHaEK?w=322&h=181&c=7&r=0&o=5&pid=1.7",
+    "title":"saiu um novo filme",
+    "description":"blá blá blá"
+  },
+  {
+    "id":"2",
+    "photo":"bbb",
+    "photoCover":"https://th.bing.com/th/id/OIP.CbdzSV3wjBjHBk13IXcECAHaEK?w=322&h=181&c=7&r=0&o=5&pid=1.7",
+    "title":"saiu uma nova série",
+    "description":"esta é a nova série do Netflix"
+  },
+  {
+    "id":"3",
+    "photo":"ccc",
+    "photoCover":"https://th.bing.com/th/id/OIP.CbdzSV3wjBjHBk13IXcECAHaEK?w=322&h=181&c=7&r=0&o=5&pid=1.7",
+    "title":"mais um lançamento nos cinemas de Portugal",
+    "description":"Novo filme da Marvel promete reviravolta"
+  },
+  {
+    "id":"4",
+    "photo":"ddd",
+    "photoCover":"https://th.bing.com/th/id/OIP.CbdzSV3wjBjHBk13IXcECAHaEK?w=322&h=181&c=7&r=0&o=5&pid=1.7",
+    "title":"mais um lançamento nos cinemas do Brasil",
+    "description":"Novo filme da Marvel promete reviravolta"
+  }
+]
